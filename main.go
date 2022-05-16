f72e95ad33a3d92b07e73a831222a851790b4744
iragsraghu <raghavendra@frontier.xyz>
2022-05-16 16:23:46 +0530

    default ports aded

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	port := os.Getenv("PORT")
 
 	router.GET("/", homePage)
 	router.GET("/api/v1/referrals", getAllDevices)
@@ -17,9 +13,5 @@ func main() {
 	router.GET("/api/v1/referral", getDevice)
 	router.GET("/api/v1/referral/counts", getReferralCounts)
 
-	if len(port) == 0 {
-		port = "8080"
-	}
-	router.Run(":" + port)
-	fmt.Println("Listening on port " + port)
+	router.Run()
 }
